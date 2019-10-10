75e290c9710a90768a33a4f33425fb1f4f6819bf
Serjio2888 <kerchtown@yandex.ru>
2019-10-11 01:20:13 +0300

    deleted cache

--- CTestTestfile.cmake
+++ CTestTestfile.cmake
@@ -5,3 +5,4 @@
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
 add_test(MainTest "/home/sergey/project/project")
+set_tests_properties(MainTest PROPERTIES  _BACKTRACE_TRIPLES "/home/sergey/project/CMakeLists.txt;21;add_test;/home/sergey/project/CMakeLists.txt;0;")
